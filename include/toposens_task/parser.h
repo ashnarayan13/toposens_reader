b3568f149d4530427541bf92446f104ed0d42edc
Ashwath Narayan Murali <ash.narayan13@gmail.com>
2021-07-08 15:41:44 +0200

    First commit

---
+++ include/toposens_task/parser.h
@@ -0,0 +1,35 @@
+
+#include <ros/ros.h>
+#include <string>
+#include<vector>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace topo
+{
+  static int val = 0;
+  class UssParser
+  {
+    public:
+      UssParser(ros::NodeHandle nh);
+      ~UssParser() {}
+
+      bool readAndPublishFrame();
+
+    private:
+
+      void parse(const std::string& frame);
+
+      float _toNum(auto &i);
+
+      void publishPcl();
+
+      pcl::PointXYZI getConvertedPoint(const pcl::PointXYZI& currPoint);
+
+      ros::Publisher _pubPcl;
+      std::string _data;
+      int _pos;
+      pcl::PointCloud<pcl::PointXYZI> _currScan;
+      unsigned long long _seq;
+  };
+}
---
+++ src/parser.cpp
@@ -0,0 +1,168 @@
+#include "toposens_task/parser.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <fstream>
+#include "pcl_conversions/pcl_conversions.h"
+
+static int temp;
+namespace topo
+{
+  UssParser::UssParser(ros::NodeHandle nh)
+  {
+    val = 0;
+    temp = 0;
+    _pubPcl = nh.advertise<sensor_msgs::PointCloud2>("/topo/pointCloud", 1);
+    std::ifstream _file("/home/nomad/catkin_ros/src/toposens_task/data/data.txt");
+    char ch;
+    while (_file >> ch)
+    {
+     _data += ch; // Or whatever
+    }
+    _pos = 0;
+  }
+
+  bool UssParser::readAndPublishFrame()
+  {
+    _currScan.clear();
+
+    std::string frame;
+    frame.clear();
+    bool toPush = false;
+    for(int i=_pos; i<_data.size(); ++i)
+    {
+      if(_data[i] == 'S')
+      {
+        toPush = true;
+        // New frame
+      }
+      if(toPush)
+      {
+        frame.push_back(_data[i]);
+      }
+      if(_data[i] == 'E')
+      {
+        toPush = false;
+        _pos = i+1;
+        break;
+      }
+    }
+
+    parse(frame);
+
+    // Publish this frame!
+    publishPcl();
+
+    if(_pos >=_data.size())
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+
+  void UssParser::parse(const std::string &frame)
+  {
+    auto i = frame.begin();
+
+    while (*i != 'S')
+    if (++i == frame.end()) return;
+
+
+    for (i; i < frame.end(); ++i)
+    {
+      // Find next X-tag in the frame
+      while (*i != 'X')
+        if (++i == frame.end()) return;
+
+      try
+      {
+        pcl::PointXYZI pcl_point;
+        pcl_point.x = _toNum(++i) / 1000.0;
+
+        if (*(++i) == 'Y')
+          pcl_point.y = _toNum(++i) / 1000.0;
+        else
+          throw std::invalid_argument("Expected Y-tag not found");
+
+        if (*(++i) == 'Z')
+          pcl_point.z = _toNum(++i) / 1000.0;
+        else
+          throw std::invalid_argument("Expected Z-tag not found");
+
+        if (*(++i) == 'V')
+          pcl_point.intensity = _toNum(++i) / 100.0;
+        else
+          throw std::invalid_argument("Expected V-tag not found");
+
+        if (pcl_point.intensity > 0)
+        {
+          // Conversion to correct coordinate frame
+          _currScan.points.push_back(getConvertedPoint(pcl_point));
+        } 
+      }
+      catch (const std::exception &e)
+      {
+        ROS_INFO("Skipped invalid point in stream");
+        ROS_DEBUG("Error: %s in message %s", e.what(), frame.c_str());
+      }
+    }
+  }
+
+  float UssParser::_toNum(auto &i)
+  {
+    // Size of X, Y, Z, V data is always 5 bytes
+    int abs = 0, factor = 1, length = 5;
+
+    // Throw exception if first character is not a number or "-"
+    if (*i == '-')
+      factor = -1;
+    else if (*i != '0')
+      throw std::invalid_argument("Invalid value char");
+
+    while (--length)
+    {
+      int d = *(++i) - '0';
+      if (d >= 0 && d <= 9)
+        abs = abs * 10 + d;
+      else
+        throw std::bad_cast();
+    }
+
+    return (float)(factor * abs);
+  }
+
+  pcl::PointXYZI UssParser::getConvertedPoint(const pcl::PointXYZI& currPoint)
+  {
+    pcl::PointXYZI pcl_point;
+    pcl_point.x = currPoint.z;
+    pcl_point.y = -currPoint.x;
+    pcl_point.z = -currPoint.y;
+    pcl_point.intensity = currPoint.intensity;
+
+    return pcl_point;
+  }
+
+  void UssParser::publishPcl()
+  {
+    sensor_msgs::PointCloud2 pclMsg;
+
+    _currScan.header.stamp = pcl_conversions::toPCL(ros::Time::now());
+    _currScan.header.seq = _seq;
+    _currScan.header.frame_id = "toposens";
+    _currScan.height = 1;
+    _seq++;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr _cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    _cloud->header.stamp = pcl_conversions::toPCL(ros::Time::now());
+    _cloud->header.frame_id = "test";
+    _cloud->header.seq = _seq;
+    _cloud->height = 1;
+    _seq++;
+
+    _currScan.width = _currScan.points.size();
+
+    pcl::toROSMsg(_currScan, pclMsg);
+
+    _pubPcl.publish(pclMsg);
+  }
+}
