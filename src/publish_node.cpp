b88c2f86d2f3386f65e828136d393e4a2cdf11e9
Ashwath Narayan Murali <ash.narayan13@gmail.com>
2021-07-11 15:49:19 +0200

    Added tests. Updated CMake and CI

--- src/publish_node.cpp
+++ src/publish_node.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "toposens_publish_node");
 
   ros::NodeHandle nh;
-  ros::Rate loop_rate(20);  // 50 Hz
+  ros::Rate loop_rate(20);  // 20 Hz
 
   topo::UssParser parserObj(nh);
 
